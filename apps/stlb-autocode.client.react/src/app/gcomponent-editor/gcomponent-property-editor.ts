a807af40d5650b288773231ae9ec4cbb517503e6
Rtv <calpwin@gmail.com>
2025-01-19 23:33:32 +0300

    improve

--- apps/stlb-autocode.client.react/src/app/gcomponent-editor/gcomponent-property-editor.ts
+++ apps/stlb-autocode.client.react/src/app/gcomponent-editor/gcomponent-property-editor.ts
@@ -6,49 +6,88 @@ import { GComponentList } from 'packages/stlb-base/src/gcomponent/gcomponent-lis
 import { StlbIocTypes } from 'packages/stlb-base/src/IoC/ioc-types';
 
 export class GComponentPropertyEditor {
-  
   private _selectedGComp?: StlbBaseGComponent;
 
+  private _container = new Container();
+  private _isDbPropertyEnabled = false;
+
   renderTo(parent: Container) {
-    var container = new Container();
-    container.width = 300;
-    container.height = (StlbGlobals.app.renderer.height / 3) * 2;
-    container.position.x = StlbGlobals.app.renderer.width - 300;
-    container.position.y = 0;
+    parent.addChild(this._container);
+
+    this._redrawProperties();
+
+    return this._container;
+  }
+
+  private _redrawProperties() {
+    this._container.removeChildren();
+
+    const padding = 10;
+    const containerComnponentPropWidth = 300;
+    const containerDbPropWidth = 900;
+    const containerComnponentPropX = StlbGlobals.app.renderer.width - containerComnponentPropWidth;
+    const containerDbPropX = StlbGlobals.app.renderer.width - containerDbPropWidth;
+    const containerHeight = (StlbGlobals.app.renderer.height / 3) * 2;
+
+    this._container.position.x = this._isDbPropertyEnabled ? containerDbPropX : containerComnponentPropX;
+    this._container.position.y = 0;
+
+    const containerBgG = new Graphics()
+      .rect(0, 0, this._isDbPropertyEnabled ? containerDbPropWidth : containerComnponentPropWidth, containerHeight)
+      .fill('white');
+    this._container.addChild(containerBgG);
+
+    const compPropertyContainer = new Container();
+    this._container.addChild(compPropertyContainer);
 
     var lineG = new Graphics()
       .moveTo(0, 0)
       .lineTo(0, (StlbGlobals.app.renderer.height / 3) * 2)
       .stroke({ color: 0xeeeeee, width: 1 });
 
-    container.addChild(lineG);
-    parent.addChild(container);
+    this._container.addChild(lineG);
+
+    // Db to Comp property button
+    const dbToCompPropBtnG = new Text({
+      text: this._isDbPropertyEnabled ? 'To DB property' : 'To Component property',
+      style: { fontSize: 14 },
+    });
+    dbToCompPropBtnG.position.x = padding;
+    dbToCompPropBtnG.position.y = padding;
+    dbToCompPropBtnG.eventMode = 'static';
+    dbToCompPropBtnG.on('click', () => {
+      this._isDbPropertyEnabled = !this._isDbPropertyEnabled;
+      this._redrawProperties();
+    });
+    this._container.addChild(dbToCompPropBtnG);
 
     let currentX = 0;
-    let currentY = container.getBounds().rectangle.height;
+    let currentY = dbToCompPropBtnG.getBounds().height + padding;
 
+    // Component property
     const w = watch(StlbStore.default.getState, 'stlbbase.selectedComponentId');
     StlbStore.default.subscribe(
       w((newVal?: string, oldVal?: string) => {
         if (!newVal) return;
 
-        const compList = StlbIoc.get<GComponentList>(
-          StlbIocTypes.GComponentList
-        );
+        const compList = StlbIoc.get<GComponentList>(StlbIocTypes.GComponentList);
         this._selectedGComp = compList.getComponentById(newVal)!;
 
-        this._selectedGComp.redrawProperty();
-        container.removeChildren();
-        container.addChild(this._selectedGComp.propertyEditorContainer);
-
-        currentY = this._selectedGComp.propertyEditorContainer.getBounds().rectangle.height;        
+        if (!this._isDbPropertyEnabled && this._selectedGComp)
+          this.redrawCompProperties(this._selectedGComp, compPropertyContainer, currentX, currentY);
       })
     );
 
-    
-
-    return container;
+    if (!this._isDbPropertyEnabled && this._selectedGComp)
+      this.redrawCompProperties(this._selectedGComp, compPropertyContainer, currentX, currentY);
   }
 
-  private _renderProperty(comp: StlbTextGComponent) {}
+  private redrawCompProperties(comp: StlbBaseGComponent, toContainer: Container, currentX: number, currentY: number) {
+    comp.redrawProperty();
+
+    toContainer.removeChildren();
+    toContainer.x = currentX;
+    toContainer.y = currentY;
+    toContainer.addChild(comp.propertyEditorContainer);
+  }
 }
